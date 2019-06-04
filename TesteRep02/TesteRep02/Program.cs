56f0e63d759122c28aaf2a085a79e7cea0aa6066
fcomarcio <fcomarcio@gmail.com>
2019-06-03 22:16:49 -0300

    subindo do vs

---
+++ TesteRep02/TesteRep02/Program.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteRep02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+            Log.Information("serilog de teste");
+
+        }
+    }
+}
